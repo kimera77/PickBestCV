c7d7e6fa91ab38b7148e983c3c3b2c08442b7208
Quim Serra <QuimSerra7@gmail.com>
2025-09-27 13:46:44 +0000

    otro

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,7 +10,6 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
             {...props}
         >
             <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Z" opacity="0.2"/>
-            <path d="M128,32a96,96,0,1,0,96,96A96.11,96.11,0,0,0,128,32Zm0,176a80,80,0,1,1,80-80A80.09,80.09,0,0,1,128,208Z" fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="16"/>
             <path d="M128,80v96" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
             <path d="M172,96a24,24,0,0,0,0,48,8,8,0,0,1,8,8v16a8,8,0,0,1-8,8,24,24,0,0,0,0,48h0a24,24,0,0,0,23.4-28.8" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
             <path d="M128,80a48,48,0,0,0-48,48v40a48,48,0,0,0,48,48" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
