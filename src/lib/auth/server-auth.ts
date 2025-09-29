66abe4b7b2942a30abe2c99e8a2f9736ffc5c686
Quim Serra <QuimSerra7@gmail.com>
2025-09-29 15:36:16 +0000

    Dos cosas. El logo que te he pasado no es el mismo que has puesto. Te pe

--- src/lib/auth/server-auth.ts
+++ src/lib/auth/server-auth.ts
@@ -3,7 +3,7 @@ import { getFirebaseAuth } from "next-firebase-auth-edge/lib/auth";
 const serviceAccount = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
 };
 
 export const serverAuth = getFirebaseAuth({
