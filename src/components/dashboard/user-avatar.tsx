a00f8906d98e95005c7a0d00f1451cdd5c381c24
Quim Serra <QuimSerra7@gmail.com>
2025-10-01 15:20:17 +0000

    el dibujo sigue siendo pequeño. Haz ese icono algo mas grande sin modifi

--- src/components/dashboard/user-avatar.tsx
+++ src/components/dashboard/user-avatar.tsx
@@ -11,7 +11,7 @@ export function UserAvatar() {
     <Avatar className="h-8 w-8">
       <AvatarImage src={user?.photoURL ?? ''} alt={user?.displayName ?? ''} />
       <AvatarFallback>
-        <CircleUser className="h-8 w-8 opacity-80" />
+        <CircleUser className="h-8 w-8 p-0.5 opacity-80" />
       </AvatarFallback>
     </Avatar>
   );
