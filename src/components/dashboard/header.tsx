a2f4b833f2b044ffff9a42ab7287c8ef4fcb3179
Quim Serra <QuimSerra7@gmail.com>
2025-10-01 18:43:40 +0000

    Sigues sin cambiar y arreglar el titulo del Dashboard, porque sigues toc

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {
   CircleUser,
+  PanelLeft,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
@@ -15,20 +16,52 @@ import LanguageSwitcher from "./language-switcher";
 import { handleSignOut } from "@/lib/auth/actions";
 import { UserAvatar } from "./user-avatar";
 import { Logo } from "@/components/logo";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 
 export default function Header() {
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background/80 backdrop-blur-sm px-4 md:px-6 z-50">
-      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+    <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6">
         <Link
           href="#"
-          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          className="flex items-center gap-2 font-semibold"
         >
           <Logo className="h-7 w-7" />
           <span className="font-bold text-xl tracking-tight">PickbestCV</span>
         </Link>
-      </nav>
+        
+        <Sheet>
+            <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="sm:hidden">
+                <PanelLeft className="h-5 w-5" />
+                <span className="sr-only">Toggle Menu</span>
+            </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="sm:max-w-xs">
+            <nav className="grid gap-6 text-lg font-medium">
+                <Link
+                href="#"
+                className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+                >
+                <Logo className="h-5 w-5 transition-all group-hover:scale-110" />
+                <span className="sr-only">PickbestCV</span>
+                </Link>
+                <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                >
+                Dashboard
+                </Link>
+                <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                >
+                Ajustes
+                </Link>
+            </nav>
+            </SheetContent>
+        </Sheet>
+
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
         <div className="ml-auto flex-1 sm:flex-initial" />
         <LanguageSwitcher />
