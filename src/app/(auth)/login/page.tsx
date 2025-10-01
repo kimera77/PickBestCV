0110acf85fd1a6234a31b9e53f76f2c30e94f9fe
Quim Serra <QuimSerra7@gmail.com>
2025-10-01 18:41:01 +0000

    Sigue sin verse

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -14,7 +14,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { AlertCircle, Loader2 } from "lucide-react";
 import {
   Form,
