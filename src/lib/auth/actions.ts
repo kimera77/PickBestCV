686c2fff5591643e237e92db634334382c51f846
Quim Serra <QuimSerra7@gmail.com>
2025-10-13 19:26:05 +0000

    Try fixing this error: `Runtime Error: Error: Failed to get tokens. Erro

--- src/lib/auth/actions.ts
+++ src/lib/auth/actions.ts
@@ -22,7 +22,7 @@ const signInSchema = z.object({
 const serviceAccount = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!,
 };
 
 const serverAuth = getFirebaseAuth({
